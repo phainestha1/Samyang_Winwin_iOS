3981de88ff38965c78fe6439b4298a1486d09570
phainestha1 <250wons@gmail.com>
2022-06-27 12:33:28 +0900

    Initial Commit

---
+++ Samyang_Winwin_iOS/Samyang_Winwin_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Samyang_Winwin_iOSApp.swift
+//  Samyang_Winwin_iOS
+//
+//  Created by Noah's Ark on 2022/06/27.
+//
+
+import SwiftUI
+
+@main
+struct Samyang_Winwin_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
