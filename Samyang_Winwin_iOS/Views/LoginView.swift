d6d8086e16dcb654e64dce001108e48586f38603
Hyeon Sang Lee <250wons@gmail.com>
2022-06-28 12:30:29 +0900

    FEAT: LoginView basic setting, set secure text component

--- Samyang_Winwin_iOS/Views/LoginView.swift
+++ Samyang_Winwin_iOS/Views/LoginView.swift
@@ -8,12 +8,17 @@
 import SwiftUI
 
 struct LoginView: View {
+    
+    //State Variables
+    @State var userId: String = ""
+    @State var password: String = ""
+    @State var isSecure: Bool = true
+    
     var body: some View {
         VStack {
-            Text("ID")
-            Text("Password")
-                .padding()
-            
+            TextField("아이디", text: $userId)
+            SecureTextToggleComponent(password: $password, isSecure: $isSecure)
+
             Button("로그인") {
                 print("Let me log in!!")
             }
@@ -24,6 +29,36 @@ struct LoginView: View {
 
 struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
-        LoginView()
+        LoginView(userId: "winwin01", password: "123123123")
     }
 }
+
+struct SecureTextToggleComponent: View {
+    
+    // State Variables
+    @Binding var password: String
+    @Binding var isSecure: Bool
+    
+    var body: some View {
+        switch isSecure {
+        case true:
+            HStack {
+                SecureField("비밀번호", text: $password)
+                Spacer()
+                Button("Click Me!") {
+                    self.isSecure.toggle()
+                }
+            }
+            
+        case false:
+            HStack {
+                TextField("비밀번호", text: $password)
+                Spacer()
+                Button("Click Me!") {
+                    self.isSecure.toggle()
+                }
+            }
+        }
+        
+    }// body
+}// PasswordComponent
